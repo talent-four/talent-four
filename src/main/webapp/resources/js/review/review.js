0aee90ee553024dc8c6bc5d76915b2fac89be87f
gunhaa <168693235+gunhaa@users.noreply.github.com>
2024-07-11 16:34:51 +0900

    240711

--- src/main/webapp/resources/js/review/review.js
+++ src/main/webapp/resources/js/review/review.js
@@ -1,9 +1,34 @@
 
 
-const writebtn = document.querySelector(".btn3");
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.radio-button');
 
-writebtn.addEventListener("click", ()=>{
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(btn => btn.classList.remove('selected'));
 
-    alert('hi');
+            button.classList.add('selected');
+
+            const value = button.getAttribute('data-value');
+            const radioButton = document.querySelector(`input[type="radio"][value="${value}"]`);
+            radioButton.checked = true;
+        });
+    });
+});
+
+
+const write = document.querySelector(".btn3");
+
+write.addEventListener("click", ()=>{
+
+    document.querySelector("#background-modal").hidden= false;
+    document.querySelector("#write-modal").hidden= false;
+
+});
+
+document.querySelector("#write-btn2").addEventListener("click", ()=>{
+
+    document.querySelector("#background-modal").hidden= true;
+    document.querySelector("#write-modal").hidden= true;    
 
 });
