319b3c90a37cf26fcaf2d11e048a5fe577755cfc
psh <psh0329m@naver.com>
2024-07-18 17:45:38 +0900

    07.18 커밋

---
+++ src/main/java/talentFour/tutor/controller/TutorClassesServlet.java
@@ -0,0 +1,17 @@
+package talentFour.tutor.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/tutor/classes")
+public class TutorClassesServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/tutor/classes.jsp").forward(req, resp);
+	}
+}
